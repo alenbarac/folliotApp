c1a78bebc3904fff5e6a48ee68a3dcac5f9e47a5
Alen <info@alenbarac.com>
2023-05-10 23:13:14 +0200

    project scope and base plan

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -16,9 +16,51 @@ export default function Dashboard(props) {
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg mb-3">
+                        <div className="p-6 text-gray-900">
+                            <h1 className="text-[22px] pb-3">Features List</h1>
+                            <hr />
+                            <ul className="mt-5">
+                                <li>Workshops CRUD</li>
+                                <li>Teachers CRUD</li>
+                                <li>Members CRUD</li>
+                                <li>Payments Data</li>
+                                <li>Calendar for workshops</li>
+                                <li>Docs import/export</li>
+                                <li>Email notifications</li>
+                                <li>Todo's/Notes</li>
+                            </ul>
+                        </div>
+                    </div>
+
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
-                            You're logged in to FoliotApp!
+                            <h1 className="text-[22px] pb-3">
+                                Backend - Migrations, Models
+                            </h1>
+                            <hr />
+                            <ul className="mt-5">
+                                <li>Workshops</li>
+                                <li>Teachers</li>
+                                <li>Members</li>
+                                <li>Payments</li>
+                                <li>Schedlue?</li>
+                                <li>Calendar</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-3">
+                        <div className="p-6 text-gray-900">
+                            <h1 className="text-[22px] pb-3">Frontend</h1>
+                            <hr />
+                            <ul className="mt-5">
+                                <li>Material UI</li>
+                                <li>React table package</li>
+                                <li>Toast library</li>
+                                <li>Calendar</li>
+                                <li>Charts</li>
+                            </ul>
                         </div>
                     </div>
                 </div>
