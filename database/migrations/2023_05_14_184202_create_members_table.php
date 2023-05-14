52ca1498e20370afeefcad73d5b4ded2f234ae7a
Alen <info@alenbarac.com>
2023-05-14 22:13:56 +0200

    members, workshops base migration tables created

---
+++ database/migrations/2023_05_14_184202_create_members_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('member_group_id')->nullable()->constrained();
+            $table->string('name');
+            $table->string('lastname');
+            $table->string('dob');
+            $table->string('parrent_name');
+            $table->string('parrent_phone');
+            $table->string('parrent_email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+};
